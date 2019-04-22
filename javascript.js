0356bf972d3c8cc9cc17e9887e6f61bcc9a69c63
Noman Azmat Ullah <41420305+NomanAzmatUllah@users.noreply.github.com>
2019-04-22 02:31:08 -0700

    Practice Faceebook Page

---
+++ javascript.js
@@ -0,0 +1,57 @@
+
+
+function expandLoris(){
+
+    var paragraph = "According to the Pakistan Cricket Boaard The Trophy Is come to the pakistan and the pakitani captain sarfraz ahmad took the trophy and showing and want to staay the trophy in pakistan"
+
+document.getElementById("worldcup").innerHTML = paragraph;
+
+}
+
+function trophy()
+{
+
+    var para = "According to the Pakistan team Cation Safraz Ahmad Show the importance of the belief thaat Pakistan Should Win";
+
+document.getElementById("cup").innerHTML = para;
+
+
+
+}
+
+
+
+function expand()
+{
+
+    var para = "India is the winner of the laast Asia Cup And i seem to be a consective history of winnig of India cricket team";
+
+document.getElementById("data").innerHTML = para;
+
+
+
+}
+
+
+function team()
+{
+
+    var para = "India is the winner of the laast Asia Cup And i seem to be a consective history of winnig of India cricket team";
+
+document.getElementById("team").innerHTML = para;
+
+
+
+}
+
+function show(){
+
+
+var a = document.getElementById('Email').value = "noman@yahoo.com";
+var b = document.getElementById('password').value = "Karachi";
+
+
+    window.location.href="./mainpage.html";
+
+
+}
